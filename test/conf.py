d253287c455343e73cdda152c966da1ee20d1219
Roland Puntaier <roland.puntaier@gmail.com>
2016-12-21 16:34:23 +0100

    conf.py which copies css into html to uncomment

--- test/conf.py
+++ test/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.abspath('..'))
 import setup as setupfile
 latex_engine = 'xelatex'
@@ -16,10 +16,8 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 html_static_path = ['_static']
 htmlhelp_basename = 'thmdoc'
-latex_elements = {
-'preamble': r'''
+latex_preamble = r"""
 %preamble for sphinxcontrib-thm
-\usepackage{amsmath}
 \usepackage{unicode-math}
 \usepackage{amsthm}%\usepackage{ntheorem} works too
 \theoremstyle{plain}
@@ -52,15 +50,13 @@ latex_elements = {
 \newtheorem{conclusion}{Conclusion}
 \newtheorem{instruction}{Instruction}%because we used .. environment:: instruction
 %\newtheorem{proof}{Proof} for ntheorem
-''',
-}
+"""
+latex_elements = {'preamble':latex_preamble}
 latex_documents = [
-  ('index', 'thm.tex', 'thm Sphinx Extension Documentation',
-   'Roland Puntaier', 'howto'),
+  ('index', 'thm.tex', 'thm Sphinx Extension Documentation','Roland Puntaier', 'howto'),
 ]
-def custom_css(app,exception):
-    with open(exclude_patterns[0]+'/html/'+html_static_path[0]+'/custom.css','a') as f:
-        f.write("""
+
+thmstyle="""
 div.thm_caption{
     padding-top: 0.3ex;
     font-weight: bold;
@@ -95,7 +91,42 @@ p.flushright{
 p.center{
     text-align: center;
 }
-        """);
+"""
+
+def custom_css(app,exception):
+    "write additional thmstyle css into custom.css"
+    with open(exclude_patterns[0]+'/html/'+html_static_path[0]+'/custom.css','a') as f:
+        f.write(thmstyle)
+
+#def custom_css(app,exception):
+#    "copy all css into the html"
+#    prefx = exclude_patterns[0]+'/html/'
+#    docf = prefx + master_doc+'.html'
+#    with open(docf,'r') as f: 
+#        doc = f.read()
+#    restsh = re.compile(r'<link rel="stylesheet" href="([^"]+)"\s+type="text/css"\s*/>')
+#    recss = re.compile(r'@import url\("([^"]+)"\);')
+#    docs = re.split(restsh,doc)
+#    for i in range(1,len(docs),2):
+#        relpth = docs[i]
+#        csspfx = relpth.rsplit('/')[0]
+#        fn = prefx + relpth
+#        if fn.find('custom.css')>0:
+#            docs[i] = "<style>\n"+thmstyle+"\n</style>";
+#        else:
+#            with open(fn,'r') as f:
+#                css = f.read()
+#                csses = re.split(recss,css)
+#                for j in range(1,len(csses),2):
+#                    ffn = prefx+csspfx+'/'+csses[i]
+#                    with open(ffn,'r') as ff:
+#                        csses[i] = ff.read()
+#                css = "\n".join(csses)
+#                docs[i] = "<style>\n"+css+"\n</style>"
+#    doc = "\n".join(docs)
+#    with open(docf,'w') as f:
+#        f.write(doc)
+
 def setup(app):
     app.connect('build-finished', custom_css)
 
